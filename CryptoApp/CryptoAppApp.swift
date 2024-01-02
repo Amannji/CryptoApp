9b8dc8a0fa3e1cccfc1b4faef29ce8b4089e0ecb
Decoy101 <00amangpt00@gmail.com>
2024-01-02 12:51:44 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Aman Gupta on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
